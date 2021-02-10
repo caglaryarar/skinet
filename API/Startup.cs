c55b5093c47d41e515caad93754a5792494d0770
caglar-yarar <caglar.yarar@ettuso.com>
2021-02-10 17:11:18 +0300

    End Of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -29,7 +30,8 @@ namespace API
         {
             services.AddControllers();
             services.AddScoped<IProductRepository, ProductRepository>();
-
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext>(options => options.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
         }
 
@@ -44,6 +46,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
